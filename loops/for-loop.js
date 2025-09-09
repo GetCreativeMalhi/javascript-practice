ab0efb21247e8d87e0fba30e6514987c8edc472f
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-09-10 08:37:01 +1000

    feat: add loops folder with for and for of loop
    - added for loop practice examples to show
    iteration with array index.
    - added for of loop practice to demonstrate array
    iteration without array index.

---
+++ loops/for-loop.js
@@ -0,0 +1,4 @@
+const cars = ["mazda", "bmw", "mitsubishi", "toyota"];
+for(let i = 0; i < cars.length; i++){
+    console.log(cars[i]);
+}
---
+++ loops/for-of-loop.js
@@ -0,0 +1,4 @@
+const cars = ["ram", "toyota", "ford", "mazda"];
+for (let car of cars){
+    console.log(car);
+}
